b2e741f8662637faeb330375766774ce513c85ed
Janishmn <157222486+Janishmn@users.noreply.github.com>
2025-10-01 10:46:58 +0200

    feat: ajout de la vérification de victoire

--- game.ts
+++ game.ts
@@ -24,4 +24,16 @@ function isColumnFull(col: number): boolean {
   return grid[0][col] !== ' ';
 }
 
+function checkVictory(): boolean {
+  // Vérification lignes
+  for (let r = 0; r < 6; r++) {
+    for (let c = 0; c < 4; c++) {
+      let val = grid[r][c];
+      if (val !== ' ' && val === grid[r][c+1] && val === grid[r][c+2] && val === grid[r][c+3]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
 
